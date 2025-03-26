ba508e49599573fc3655080ecb78fb8e6130d357
Bhupesh sharma <87887627+Bhupesh2sharma@users.noreply.github.com>
2025-03-26 08:58:16 +0530

    Update razorpay.js

--- utils/razorpay.js
+++ utils/razorpay.js
@@ -2,7 +2,7 @@ const Razorpay = require('razorpay');
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret:XFEEyKQyMZav77JnfRIOTWLR
+  key_secret:wPrSd4Nvd1DvqrSsGHMPHvB8
 });
 
 module.exports = razorpay;
